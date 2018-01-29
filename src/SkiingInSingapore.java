c653671908360375fb96199f5b0919d9451f34a5
Harsh Gupta <harsh.gupta@snapdeal.com>
2018-01-29 16:43:03 +0530

    Formatting and commenting !

--- src/SkiingInSingapore.java
+++ src/SkiingInSingapore.java
@@ -34,32 +34,40 @@ public class SkiingInSingapore {
         this.largestDrop = largestDrop;
     }
 
-    public void solveLongestGrid(String pathToFile){
+    /**
+     *
+     * @param pathToFile
+     *
+     * This function solves the problem.
+     * 1. It takes the path of the file and parses it into a matrix.
+     * 2. Applies DFS to it's cells and stores the longest path and largest drop.
+     */
+    public void solveLongestGrid(String pathToFile) {
         int mountainMatrix[][];
         try {
             mountainMatrix = parseDataFromFile(pathToFile);
-        } catch (IOException  | URISyntaxException e) {
+        } catch (IOException | URISyntaxException e) {
             System.out.println("Exception while Parsing file");
             return;
         }
         // Util Matrix to traverse in four directions : East, West, North , South
-        int util [][] = {
-                { 0, -1, 1, 0 },
-                { 1, 0, 0, -1 }
+        int util[][] = {
+                {0, -1, 1, 0},
+                {1, 0, 0, -1}
         };
 
         // Traversing through every cell and  keeping a check on longest path and largest drop
-        for(int i=0;i<mountainMatrix.length;i++){
-            for(int j=0; j< mountainMatrix[0].length;j++){
-                List<Grid> currentLongestPath = getLongestSkiingPathFromGrid(i,j,mountainMatrix,util);
-                if(currentLongestPath.size() > longestPath){
+        for (int i = 0; i < mountainMatrix.length; i++) {
+            for (int j = 0; j < mountainMatrix[0].length; j++) {
+                List<Grid> currentLongestPath = getLongestSkiingPathFromGrid(i, j, mountainMatrix, util);
+                if (currentLongestPath.size() > longestPath) {
                     longestPath = currentLongestPath.size();
                     largestDrop = mountainMatrix[currentLongestPath.get(0).getX()][currentLongestPath.get(0).getY()] -
-                                    mountainMatrix[currentLongestPath.get(currentLongestPath.size() -1).getX()][currentLongestPath.get(currentLongestPath.size() -1).getY()];
+                            mountainMatrix[currentLongestPath.get(currentLongestPath.size() - 1).getX()][currentLongestPath.get(currentLongestPath.size() - 1).getY()];
 
-                } else if(currentLongestPath.size() == longestPath){
+                } else if (currentLongestPath.size() == longestPath) {
                     int currentDrop = mountainMatrix[currentLongestPath.get(0).getX()][currentLongestPath.get(0).getY()] -
-                            mountainMatrix[currentLongestPath.get(currentLongestPath.size() -1).getX()][currentLongestPath.get(currentLongestPath.size() -1).getY()];
+                            mountainMatrix[currentLongestPath.get(currentLongestPath.size() - 1).getX()][currentLongestPath.get(currentLongestPath.size() - 1).getY()];
                     largestDrop = currentDrop > largestDrop ? currentDrop : largestDrop;
 
                 }
@@ -69,7 +77,6 @@ public class SkiingInSingapore {
     }
 
     /**
-     *
      * @param row
      * @param column
      * @param mountainMatrix
@@ -79,38 +86,37 @@ public class SkiingInSingapore {
      * @return Boolean value
      * LOGIC : Returns true if the grid (mountain slope ) is allowed from the last grid.
      */
-    private boolean isGridAllowed(int row,int column, int mountainMatrix[][], int totalRows, int totalColumns, int currentValue){
-        return (row >=0 && row < totalRows
-                && column >=0 && column < totalColumns
+    private boolean isGridAllowed(int row, int column, int mountainMatrix[][], int totalRows, int totalColumns, int currentValue) {
+        return (row >= 0 && row < totalRows
+                && column >= 0 && column < totalColumns
                 && mountainMatrix[row][column] < currentValue);
     }
 
     /**
-     *
      * @param x
      * @param y
      * @param mountainMatrix
      * @param util
      * @return List of Grid having the longest path from x,y( row, column )
-     *
+     * <p>
      * This uses the DFS technique to find the longest path and the largest drop
      */
-    private List<Grid> getLongestSkiingPathFromGrid(int x, int y, int mountainMatrix[][], int util[][]){
+    private List<Grid> getLongestSkiingPathFromGrid(int x, int y, int mountainMatrix[][], int util[][]) {
 
         List<Grid> currentSkiingPath = new ArrayList<>();
 
-        for(int i=0;i<4;i++){
-            if(isGridAllowed(x+util[0][i], y+util[1][i],mountainMatrix,mountainMatrix.length, mountainMatrix[x].length,mountainMatrix[x][y])){
-                ArrayList<Grid> tempSkiingPath =(ArrayList<Grid>) getLongestSkiingPathFromGrid(x+util[0][i], y+util[1][i], mountainMatrix,util);
-                if(tempSkiingPath.size() > currentSkiingPath.size()){
+        for (int i = 0; i < 4; i++) {
+            if (isGridAllowed(x + util[0][i], y + util[1][i], mountainMatrix, mountainMatrix.length, mountainMatrix[x].length, mountainMatrix[x][y])) {
+                ArrayList<Grid> tempSkiingPath = (ArrayList<Grid>) getLongestSkiingPathFromGrid(x + util[0][i], y + util[1][i], mountainMatrix, util);
+                if (tempSkiingPath.size() > currentSkiingPath.size()) {
                     // This check passes when we find a longer path than the previous one.
                     currentSkiingPath = tempSkiingPath;
-                } else if(tempSkiingPath.size() == currentSkiingPath.size()){
-                        // This step is to use the minimum value of the two grids in comparisons
-                        currentSkiingPath =
-                                mountainMatrix[tempSkiingPath.get(tempSkiingPath.size()-1).getX()][tempSkiingPath.get(tempSkiingPath.size()-1).getY()] <
-                                        mountainMatrix[currentSkiingPath.get(currentSkiingPath.size()-1).getX()][currentSkiingPath.get(currentSkiingPath.size()-1).getY()] ?
-                                        tempSkiingPath : currentSkiingPath;
+                } else if (tempSkiingPath.size() == currentSkiingPath.size()) {
+                    // This step is to choose the gridlist with larger drop
+                    currentSkiingPath =
+                            mountainMatrix[tempSkiingPath.get(tempSkiingPath.size() - 1).getX()][tempSkiingPath.get(tempSkiingPath.size() - 1).getY()] <
+                                    mountainMatrix[currentSkiingPath.get(currentSkiingPath.size() - 1).getX()][currentSkiingPath.get(currentSkiingPath.size() - 1).getY()] ?
+                                    tempSkiingPath : currentSkiingPath;
 
                 }
             }
@@ -118,22 +124,19 @@ public class SkiingInSingapore {
 
 
         List<Grid> longestSkiingPath = new ArrayList<>();
-        longestSkiingPath.add(new Grid(x,y));
+        longestSkiingPath.add(new Grid(x, y));
         longestSkiingPath.addAll(currentSkiingPath);
         return longestSkiingPath;
 
     }
 
     /**
-     *
      * @param pathToFile
      * @return
      * @throws IOException
-     * @throws URISyntaxException
-     *
-     * A util function to parse data from the file into a matrix
+     * @throws URISyntaxException A util function to parse data from the file into a matrix
      */
-    private int[][] parseDataFromFile(String pathToFile) throws IOException,URISyntaxException{
+    private int[][] parseDataFromFile(String pathToFile) throws IOException, URISyntaxException {
 
         int mountainMatrix[][];
 
@@ -149,12 +152,12 @@ public class SkiingInSingapore {
         mountainMatrix = new int[rows][columns];
 
         Iterator it = list.iterator();
-        if(it.hasNext()){
+        if (it.hasNext()) {
             it.next(); // skipping the grid size line
         }
 
-        int currentRow =0;
-        while(it.hasNext()){
+        int currentRow = 0;
+        while (it.hasNext()) {
 
             String rowLine = (String) it.next();
             String columnValues[] = rowLine.split(TEXT_DELIMITER);
@@ -162,17 +165,17 @@ public class SkiingInSingapore {
             int currentColumn = 0;
 
             // parsing every line into a corresponding grid
-            for(String column : columnValues){
-                    mountainMatrix[currentRow][currentColumn++] = Integer.valueOf(column);
+            for (String column : columnValues) {
+                mountainMatrix[currentRow][currentColumn++] = Integer.valueOf(column);
             }
             currentRow++;
         }
         return mountainMatrix;
     }
 
-    private void printPath(List<Grid> list, int mountain[][]){
-        for(Grid grid : list){
-            System.out.println("X: Y: ("+grid.getX()+","+grid.getY()+"), Value : "+mountain[grid.getX()][grid.getY()]);
+    private void printPath(List<Grid> list, int mountain[][]) {
+        for (Grid grid : list) {
+            System.out.println("X: Y: (" + grid.getX() + "," + grid.getY() + "), Value : " + mountain[grid.getX()][grid.getY()]);
         }
     }
 
